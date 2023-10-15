1a29e09595480ead3a2a3ff9ef5032adb4fa628f
hokageCV <chaitanyavaru@gmail.com>
2023-10-15 22:53:16 +0530

    add cors

--- src/app/api/contact-form/route.js
+++ src/app/api/contact-form/route.js
@@ -2,7 +2,7 @@ import sendEmail from './sendEmail';
 
 const allowedOrigin = 'https://chaitanyavaru.me';
 
-export async function POST(req) {
+async function handleRequest(req) {
     const origin = req.headers.get('Origin');
     if (origin !== allowedOrigin) {
         return new Response('Invalid origin', { status: 403 });
@@ -22,4 +22,25 @@ export async function POST(req) {
             'Access-Control-Allow-Headers': 'Content-Type',
         },
     });
+}
+
+const allowCors = (fn) => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,POST');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+    return await fn(req, res);
+};
+
+const combinedHandler = allowCors(handleRequest);
+
+export async function POST(req) {
+    return await combinedHandler(req);
 }
