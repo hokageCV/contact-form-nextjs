ee9cf2d8fa2bca6f474e84bdbe07a03f3c665f01
hokageCV <chaitanyavaru@gmail.com>
2023-10-21 20:19:14 +0530

    update request handlers

--- src/app/api/contact-form/route.js
+++ src/app/api/contact-form/route.js
@@ -1,13 +1,16 @@
 import sendEmail from './sendEmail';
 
-const allowedOrigin = 'https://chaitanyavaru.me';
+const corsHeaders = new Headers({
+    'Access-Control-Allow-Origin': 'https://chaitanyavaru.me',
+    'Access-Control-Allow-Methods': 'POST',
+    'Access-Control-Allow-Headers': 'Content-Type',
+});
 
-async function handleRequest(req) {
-    const origin = req.headers.get('Origin');
-    if (origin !== allowedOrigin) {
-        return new Response('Invalid origin', { status: 403 });
-    }
+export async function OPTIONS(req) {
+    return new Response(null, { headers: corsHeaders });
+}
 
+export async function POST(req) {
     const formData = await req.json();
     const { name, email, message } = formData;
 
@@ -15,32 +18,6 @@ async function handleRequest(req) {
 
     return new Response(emailResponse.body, {
         status: emailResponse.status,
-        headers: {
-            'Content-Type': 'text/plain',
-            'Access-Control-Allow-Origin': allowedOrigin,
-            'Access-Control-Allow-Methods': 'POST',
-            'Access-Control-Allow-Headers': 'Content-Type',
-        },
+        headers: corsHeaders,
     });
-}
-
-const allowCors = (fn) => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,POST');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
-    return await fn(req, res);
-};
-
-const combinedHandler = allowCors(handleRequest);
-
-export async function POST(req) {
-    return await combinedHandler(req);
 }
