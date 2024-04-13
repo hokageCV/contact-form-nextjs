0408f6580fd221281a41d85898e38ae958e00074
hokageCV <chaitanyavaru@gmail.com>
2024-04-13 15:26:34 +0530

    Allow multiple cors origin

--- src/app/api/contact-form/route.js
+++ src/app/api/contact-form/route.js
@@ -1,23 +1,36 @@
-import sendEmail from './sendEmail';
+import sendEmail from './sendEmail'
 
-const corsHeaders = new Headers({
-    'Access-Control-Allow-Origin': 'https://chaitanyavaru.me',
+const allowedOrigins = ['https://hokagecv.github.io', 'https://chaitanyavaru.me']
+
+function corsHeaders(origin) {
+  return {
+    'Access-Control-Allow-Origin': origin,
     'Access-Control-Allow-Methods': 'POST',
-    'Access-Control-Allow-Headers': 'Content-Type',
-});
+    'Access-Control-Allow-Headers': '*',
+  }
+}
+
+function checkOrigin(req) {
+  const origin = req.headers.get('Origin')
+  const foundOrigin = allowedOrigins.find((allowedOrigin) => allowedOrigin.includes(origin))
+
+  return foundOrigin ? foundOrigin : allowedOrigins[0]
+}
 
 export async function OPTIONS(req) {
-    return new Response(null, { headers: corsHeaders });
+  const allowedOrigin = checkOrigin(req)
+  return new Response(null, { headers: corsHeaders(allowedOrigin) })
 }
 
 export async function POST(req) {
-    const formData = await req.json();
-    const { name, email, message } = formData;
+  const allowedOrigin = checkOrigin(req)
+  const formData = await req.json()
+  const { name, email, message } = formData
 
-    const emailResponse = await sendEmail(name, email, message);
+  const emailResponse = await sendEmail(name, email, message)
 
-    return new Response(emailResponse.body, {
-        status: emailResponse.status,
-        headers: corsHeaders,
-    });
+  return new Response(emailResponse.body, {
+    status: emailResponse.status,
+    headers: corsHeaders(allowedOrigin),
+  })
 }
